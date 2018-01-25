bd286b839795866000b24ab05a9ff56ac9c912c5
Ertron <ertron@veldrin.kiev.ua>
2018-01-25 15:56:45 +0200

    SetUp Update

--- tests/api/Model/AdminPanelModelTest.php
+++ tests/api/Model/AdminPanelModelTest.php
@@ -7,26 +7,34 @@ use PHPUnit\Framework\TestCase;
 
 class AdminPanelModelTest extends TestCase {
 
-	protected $adminPanel;
+	protected static $adminPanel;
 
-	protected function setUp() {
+	protected $host_name = 'test1.com';
+
+	protected $host_id = 4;
+
+	protected static $dbh;
+
+	public static function setUpBeforeClass()
+	{
 		$app = require_once __DIR__.'/../Model/../../../bootstrap.php';
-		$this->adminPanel = new AdminPanelModel($app['db']);
-	}
-
-	protected function tearDown() {
-		$this->adminPanel = null;
+		self::$adminPanel = new AdminPanelModel($app['db']);
 	}
 
 	/**
 	 * @test method isSetAdminPanel
 	 */
 	public function testIsSetAdminPanel(){
-		$host_name = 'test1.com';
-		$host_id = 4;
 
-		$this->assertTrue($this->adminPanel->isSetAdminPanel(null, $host_id));
-		$this->assertTrue($this->adminPanel->isSetAdminPanel($host_name, null));
+		$this->assertTrue(self::$adminPanel->isSetAdminPanel(null, $this->host_id));
+		$this->assertTrue(self::$adminPanel->isSetAdminPanel($this->host_name, null));
+	}
+
+	/**
+	 * @test method getAdminPanelIdByHostname
+	 */
+	public function testGetAdminPanelIdByHostname(){
+		$this->assertEquals($this->host_id, self::$adminPanel->getAdminPanelIdByHostname($this->host_name));
 	}
 
 }
